185139749992709d71e406b5ba5942a5713449c0
amarjit kumar <amarjit.kumar.11032001@gmail.com>
2026-04-20 19:52:47 +0530

    Building the video Statistics script P-1

--- video_stats.py
+++ video_stats.py
@@ -2,11 +2,27 @@ import requests
 import json
 API_KEY = "AIzaSyB5co_7gDJxYdZKGxXEJilvMPOFNKDf9_I"
 CHANNEL_HANDLE= "MrBeast"
-url =   f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
 
-response = requests.get(url)
+def get_playlist_id():
+	try:
 
-print(response)
+		url =   f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
 
-data=response.json()
-print(json.dumps(data, indent=4))
+		response = requests.get(url)
+
+		response.raise_for_status()
+
+		data=response.json()
+		#print(json.dumps(data, indent=4))
+		channel_items =data['items'][0]
+
+		channel_playlistId =channel_items['contentDetails']['relatedPlaylists']['uploads']
+
+		#print(channel_playlistId)
+		return channel_playlistId
+	
+	except requests.exceptions.RequestException as e:
+		raise e
+
+if __name__=="__main__":
+	get_playlist_id()
